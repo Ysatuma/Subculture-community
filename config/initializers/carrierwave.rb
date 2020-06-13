1e7b3e263871a69c62c71625e42c311f9a7645e9
Ysatuma <yssatuma@gmail.com>
2020-06-13 21:30:02 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,17 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'  #=> 追加
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1' # S3バケット作成時に指定したリージョン。左記は東京を指す
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV[''],     # 例（東京）: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['SECRET_ACCESS_KEY']
     }
-    config.fog_directory  = 'original-trip-sns' # 作成したS3バケット名
+    config.fog_directory     =  ENV['S3_BUCKET']
+    # キャッシュの保存期間
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end
