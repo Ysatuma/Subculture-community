a2e9d3d2afd4abafda0f27810dd99a90384075ce
Ysatuma <yssatuma@gmail.com>
2020-06-13 10:14:10 +0900

    Allow null to thumb column
    videoテーブルのthumbカラムに対し、not null制約を解除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_005031) do
+ActiveRecord::Schema.define(version: 2020_06_13_011224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2020_06_13_005031) do
 
   create_table "videos", force: :cascade do |t|
     t.string "content", null: false
-    t.string "thumb", null: false
+    t.string "thumb"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
